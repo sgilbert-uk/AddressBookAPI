34104fb27a58fde8a6eda9bc8fc23aeac095cddf
sgilbert-uk <samgilbertuk@outlook.com>
2022-03-31 10:30:44 +0100

    Added missing gradle files and h2 database setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO USERS (first_name,last_name,address_line,city,county,postcode) VALUES
+('Edsger','Djikstra','123 Fake Street','London','Surrey','CR0 6LP'),
+('Steve','Wozniak','9807 The Green','Croydon','Surrey','CR45 3NJ'),
+('Charles','Babbage','61 Windsor Road','Durham','Durham','DH12 6ZH'),
+('Ada','Lovelace', '54 North Street','Stockport','Greater Manchester','SK54 0MH'),
+('Alan','Turing','787 Albert Road','Sheffield','Sheffield','S0 4YH');
